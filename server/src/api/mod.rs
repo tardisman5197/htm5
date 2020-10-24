d2510b4ef37234385345a9618cad4d0c4586f12c
Ed <tardisman5197@users.noreply.github.com>
2020-10-24 11:02:53 +0100

    Basic api server (#4)
    * Created the API Routes
    
    This is a unclean version. But all of the endpoints are reachable and the correct methods.
    In the end I will want to split things into modules and add handler functions.
    An ok first draft.
    
    * Put things in modules
    
    * Created api module
    
    This module contians pretty much everything. It has a few more nested modules within it.
    
    * Better api returns
    
    Now using warp::reply::with_status. This allows a response to contrain a status code and
    message. There is also a reply fucntion with will send json, this will be needed later.

---
+++ server/src/api/mod.rs
@@ -0,0 +1,4 @@
+// api module has nested modules
+// access these by doing api::handlers
+pub mod handlers;
+pub mod filters;
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+mod api;
+
+#[tokio::main]
+async fn main() {
+    // Read in argument for port
+    // TODO
+
+    // Get the routes
+    let routes = api::filters::routes();
+
+    // Listen for requests
+    println!("Starting Server");
+    warp::serve(routes).run(([0,0,0,0],8080)).await;
 }
